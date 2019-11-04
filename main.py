d86ae0bcf9a7a6f68c718c4bdbd95d0f5a3c1a36
artmukr <artmukr@gmail.com>
2019-11-04 23:02:48 +0200

    Homework_20

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-
-#   Also, you need implement a custom exception WrongArtistError which is
-#   raised when you try to add a song to an album and artists don't match.
+import datetime
+from functools import reduce
+from typing import List
 
 
 class WrongArtistError(Exception):
@@ -8,7 +8,7 @@ class WrongArtistError(Exception):
 
 
 class Artist:
-    def __init__(self, name, country):
+    def __init__(self, name: str, country: str):
         self.name = name
         self.country = country
         self.songs = []
@@ -30,15 +30,22 @@ class Artist:
     def album_number(self):
         return len(self.albums)
 
+    def __repr__(self):
+        return self.name
+
 
 class Album:
-    def __init__(self, name, year, genre, artist: Artist):
+    def __init__(self, name: str, year: int, genre: str, artist: Artist):
+
         self.name = name
         self.year = year
         self.genre = genre
         self.artist = artist
         self.songs = []
         artist.add_album(self)
+
+    def __repr__(self):
+        return f'{self.name} by {self.artist.name}'
 
     def add_song(self, song):
         if song in self.songs or song.artist != self.artist:
@@ -51,44 +58,51 @@ class Album:
 
     @property
     def duration(self):
-        duration = 0
-        for song in self.songs:
-            duration += song.duration
-        return duration
+        return reduce(lambda x, y: x.duration + y.duration, self.songs)
+
+    # @property
+    # def duration(self):
+    #     return datetime.timedelta(
+    #         seconds = sum(song.duration.seconds for song in self.songs)
+    #     )
 
 
 class Song:
-    def __init__(self, name, year, duration, artist, album=None,):
+    def __init__(self, name: str, year: int, duration: int, artist: Artist,
+                 album: Album = None, features: List[Artist] = None):
+
         self.name = name
         self.year = year
         self.artist = artist
-        self.duration = duration
+        self.duration = datetime.timedelta(seconds=duration)
         self.album = album
-        self.author = []
-        self.author.append(artist)
+        self.features = features or []
+        self.features.append(artist)
         artist.add_song(self)
-        if album is not None:
-            album.add_song(self)
+        if self.album:
+            if self.album.artist != artist:
+                raise WrongArtistError(f'{album.artist} is not {artist}')
+            self.album.songs.append(self)
 
     def add_artist(self, artist):
-        if artist not in self.author:
-            return self.author.append(artist)
+        if artist not in self.features:
+            return self.features.append(artist)
 
 
 art1 = Artist("Mettalica", "USA")
 alb1 = Album('Death magnetic', 2008, 'Thrash metal', art1)
-song1 = Song('That Was Just Your Life', 2008, 7.1, art1, alb1)
-song2 = Song('The End of the Line', 2008, 7.86,  art1, alb1)
+song1 = Song('That Was Just Your Life', 2008, 426, art1, alb1)
+song2 = Song('The End of the Line', 2008, 470,  art1, alb1)
 alb2 = Album('St. Anger', 2003, 'Thrash metal', art1)
-song3 = Song('Frantic', 2003, 5.8,  art1, alb2)
+song3 = Song('Frantic', 2003, 348,  art1, alb2)
 art2 = Artist('Motorhead', "USA")
-song4 = Song('Enter Sandman', 1991, 5.5, art1)
+song4 = Song('Enter Sandman', 1991, 330, art1)
 song4.add_artist(art2)
-alb3 = Album("Motorhead", 1977, "Hard Rock", art2)
-song5 = Song("Motorhead", 1977, 3.2, art2, alb1)
+# alb3 = Album("Motorhead", 1977, "Hard Rock", art2)
+# song5 = Song("Motorhead", 1977, 192, art2, alb1)
 
 print(art1.songs_number, alb1.duration,
       alb1.songs_number, art1.album_number,
-      song4.author, art2.album_number)
+      song4.features, art2.album_number)
 
 
